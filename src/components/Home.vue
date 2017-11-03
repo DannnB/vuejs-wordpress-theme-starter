31d750fa36533317c7d0a7413685779e69e2d802
DanB <daniel@citcom.co.uk>
2017-11-03 12:07:44 +0000

    Move HTML around to make new layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,36 +1,37 @@
 <template>
     <b-container class="bv-example-row">
-        <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
-        <p>For more information visit website</p>
-        <b-btn variant="primary" href="https://bootstrap-vue.js.org" target="_blank">More Info</b-btn>
-        </b-jumbotron>
-
-        <b-row>
-            <b-col>
-                <recent-posts-widget limit="5">Recent Posts</recent-posts-widget>
-            </b-col>
-            <b-col>
-                <pages-widget limit="5">Pages</pages-widget>
-            </b-col>
-            <b-col>
-                <h3>Welcome!</h3>
-                <p>You'll probably want to remove all of this stuff, but hey it at least gives you a preview of Bootstrap-Vue!</p>
-                <p>I would encourage you to avoid using jQuery and instead find the Vue way of doing some things. You'll thank me later!</p>
-                <p>~ <a href="http://evanagee.com/">Evan Agee</a></p>
-            </b-col>
-        </b-row>
+			<b-row>
+				<b-col>
+					<h1>{{ page.title }}</h1>
+				</b-col>
+			</b-row>
+			<b-row>
+				<b-col>
+					<h2>Heading</h2>
+					<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+					<p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+				</b-col>
+				<b-col>
+				</b-col>
+				<b-col>
+				</b-col>
+				<b-col>
+				</b-col>
+			</b-row>
     </b-container>
 </template>
 
 <script>
 import RecentPostsWidget from './widgets/RecentPosts'
-import PagesWidget from './widgets/Pages'
 
 export default {
-    components: {
-        RecentPostsWidget,
-        PagesWidget
-    }
+	data() {
+		return {
+			page: {
+				title: 'Main Page'
+			}
+		}
+	}
 }
 </script>
 
--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,5 +1,26 @@
 <template>
-    <footer class="footer">
-        This is the footer.
-    </footer>
+	<footer class="footer">
+		<b-container class="bv-example-row">
+			<b-row>
+				<b-col>
+					<pages-widget limit="5">Pages</pages-widget>
+				</b-col>
+				<b-col>
+					<recent-posts-widget limit="5">Recent Posts</recent-posts-widget>
+				</b-col>
+			</b-row>
+		</b-container>
+	</footer>
 </template>
+
+<script>
+import RecentPostsWidget from './../widgets/RecentPosts'
+import PagesWidget from './../widgets/Pages'
+
+export default {
+  components: {
+		RecentPostsWidget,
+		PagesWidget
+	}
+}
+</script>
